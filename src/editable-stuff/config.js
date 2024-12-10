a1153ad02c1393e191d848826e976b506f923ca8
Pushpak2861 <pushpak2861@gmail.com>
2024-12-10 16:53:50 +0000

    name change

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,9 +6,9 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Pushpak",
+  firstName: "Pushpak Raj",
   middleName: "",
-  lastName: "Raj",
+  lastName: "Senthil Kumaran",
   message: " Time waits for no one. ",
   icons: [
     {
